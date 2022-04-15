de390f27835d6a594b99caad59743b64329185b1
mateus <mateusgf00@gmail.com>
2022-04-14 23:32:50 -0300

    create a home screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Notification, Section } from 'rbx';
+import './App.scss';
+import HomeScreen from './screens/home/index.js';
 
-const App = () =>{<p>javascript note</p>}
+const App = () => (
+    <Fragment>
+        <HomeScreen></HomeScreen>
+    </Fragment>
+);
 
 export default App;
